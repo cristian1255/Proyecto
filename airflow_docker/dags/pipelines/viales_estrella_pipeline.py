74e2b9bd24d4906bc24dbc4a7a74e6fe354d3e7f
cristian1255 <cmeneses394@gmail.com>
2026-04-23 09:12:45 -0600

    Update viales_estrella_pipeline.py

--- airflow_docker/dags/pipelines/viales_estrella_pipeline.py
+++ airflow_docker/dags/pipelines/viales_estrella_pipeline.py
@@ -1,34 +1,7 @@
-<<<<<<< HEAD
-import logging
-
-from airflow-docker.dags.etls.etl_viales_estrella import extract_viales, load_viales
-logger = logging.getLogger("airflow.task")
-
-
-# =========================
-# PIPELINE VIALES
-# =========================
-def run_viales_pipeline(**kwargs):
-    url = kwargs.get("url")
-    year = kwargs.get("year")
-
-    logger.info(f"Procesando VIALES: {url}")
-
-    data = extract_viales(url)
-
-    if not data:
-        logger.warning("Sin datos viales")
-        return "Sin datos viales"
-
-    load_viales(data, year)
-
-    return f"{len(data)} registros viales cargados"
-
-
-=======
 import logging
 
 from etls.etl_viales_estrella import extract_viales, load_viales
+
 logger = logging.getLogger("airflow.task")
 
 
@@ -50,6 +23,3 @@ def run_viales_pipeline(**kwargs):
     load_viales(data, year)
 
     return f"{len(data)} registros viales cargados"
-
-
->>>>>>> 65d3523 (primer commit airflow)

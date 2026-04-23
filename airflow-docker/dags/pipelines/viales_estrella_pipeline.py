6de57abf26a5daacb4fb871991eff6bb8c33d256
cristian1255 <cmeneses394@gmail.com>
2026-04-23 09:36:30 -0600

    Update viales_estrella_pipeline.py

--- airflow-docker/dags/pipelines/viales_estrella_pipeline.py
+++ airflow-docker/dags/pipelines/viales_estrella_pipeline.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import logging
 
-from etls.etl_viales_estrella import extract_viales, load_viales
+from dags.etls.etl_viales_estrella import extract_viales, load_viales
+
 logger = logging.getLogger("airflow.task")
 
 
@@ -14,42 +14,16 @@ def run_viales_pipeline(**kwargs):
 
     logger.info(f"Procesando VIALES: {url}")
 
+    # Extraer datos
     data = extract_viales(url)
 
     if not data:
         logger.warning("Sin datos viales")
         return "Sin datos viales"
 
+    # Cargar datos
     load_viales(data, year)
 
-    return f"{len(data)} registros viales cargados"
-
-
-=======
-import logging
-
-from etls.etl_viales_estrella import extract_viales, load_viales
-logger = logging.getLogger("airflow.task")
-
-
-# =========================
-# PIPELINE VIALES
-# =========================
-def run_viales_pipeline(**kwargs):
-    url = kwargs.get("url")
-    year = kwargs.get("year")
-
-    logger.info(f"Procesando VIALES: {url}")
-
-    data = extract_viales(url)
-
-    if not data:
-        logger.warning("Sin datos viales")
-        return "Sin datos viales"
-
-    load_viales(data, year)
+    logger.info(f"{len(data)} registros viales cargados")
 
     return f"{len(data)} registros viales cargados"
-
-
->>>>>>> 65d3523 (primer commit airflow)
